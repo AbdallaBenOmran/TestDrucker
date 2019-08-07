1952a266ad301c92ddd9979862e8e3ac2c47e5f0
Ben Omran <b.omran@fahrrad-xxl.de>
2019-08-07 16:12:15 +0200

    Update7_8

--- XUnitTestDrucker/UnitTest1.cs
+++ XUnitTestDrucker/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Configuration;
+using System.IO;
 using TestDrucker.Controllers.PrinterCanon;
 using TestDrucker.Models.PrinterCanon;
 using TestDrucker.Models.TheQ;
@@ -10,15 +11,31 @@ namespace XUnitTestDrucker
 {
     public class UnitTest1
     {
+        public IConfiguration _configuration;
+        DBQueue dbQ;
+
+        public UnitTest1()
+        {
+            CreateConfiguration();
+            dbQ = new DBQueue(_configuration.GetConnectionString("ServiceDbLive"), _configuration.GetConnectionString("ServiceDbTest"));
+
+        }
+
+        public void CreateConfiguration()
+        {
+            var configPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\", "appsettings.json"));
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile(configPath)
+                .Build();
+        }
+
         [Fact]
         public void Test1()
         {
+            //DBQueue dbQ = new DBQueue();
+            var res = dbQ.AddId("canon", "C");
+            
 
-            //DBQueue dB = new DBQueue();
-            //dB.AddId("canon", "C");
         }
-
-    
-    
     }
 }
